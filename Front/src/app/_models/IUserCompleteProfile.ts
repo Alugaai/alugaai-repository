33335235cf3bf4660b38d9392012009bcf8e5344
Joao Henrique <joaoadsistemas@gmail.com>
2024-04-25 17:08:09 -0300

    start to build form student complete profile

---
+++ Front/src/app/_models/IUserCompleteProfile.ts
@@ -0,0 +1,6 @@
+export interface IUserCompleteProfile {
+  username: string;
+  gender: string;
+  birthDate: Date | string;
+  phoneNumber: string;
+}
--- Front/src/app/complete-profile/student-complete-profile/student-complete-profile.component.ts
+++ Front/src/app/complete-profile/student-complete-profile/student-complete-profile.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from 'express';
+import { IStudentCompleteProfile } from '../../_models/IStudentCompleteProfile';
+import { AuthService } from '../../_services/auth.service';
+import { IUserDetails } from '../../_models/IUserDetails';
 
 @Component({
   selector: 'app-student-complete-profile',
@@ -7,7 +12,47 @@ import { Component } from '@angular/core';
 })
 export class StudentCompleteProfileComponent {
 
-  
+  userLogged: boolean = false;
+  emailUserLogged: string = '';
+  user?: IUserDetails;
+  student?: IStudentCompleteProfile;
+  studentForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder,
+    private router: Router, private authService: AuthService) { this.authService.userLoggedToken$.subscribe({
+      next: (userToken) => {
+        this.userLogged = userToken !== null ? true : false;
+        if (userToken?.email) {
+          this.emailUserLogged = userToken.email;
+        }
+      },
+    });}
+
+    ngOnInit(): void {
+
+      this.authService.userDetailsByEmail(this.emailUserLogged).subscribe({
+        next: (response) => {
+          this.user = response;
+        },
+        error: (error) => {
+          console.error(error);
+        },
+      });
+
+      this.studentForm = this.fb.group({
+        username: ['', [Validators.maxLength(250)]],
+        gender: ['', [Validators.required]],
+        birthDate: [''],
+        phoneNumber: [
+          '',
+          [
+            Validators.minLength(11),
+            Validators.maxLength(11),
+          ],
+        ],
+        collegeId: [ undefined, [Validators.required]],
+      });
+    }
 
 
 }
